4e5e3742c17640f0a34d2a1ea964e0796a320dbf
Nightowl <63543943+Nightowl914@users.noreply.github.com>
2024-01-13 02:41:45 +0800

    Updated main.py
    Updated code into functions for better readability

--- main.py
+++ main.py
@@ -6,32 +6,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+# Function that initialize the driver
+def initialize_driver():
+    service = Service(executable_path="chromedriver.exe")
+    return webdriver.Chrome(service=service)
 
-# Enter a search term
-user_input = input("Enter a search term: ")
+# Function that take in user input and search for the product
+def search_product(driver, user_input):
+    driver.get("https://www.automationexercise.com/products")
+    
+    # Target the search box by id
+    search_input = driver.find_element(By.ID, "search_product")
+    # Take in user input and append it on the search bar
+    search_input.send_keys(user_input)
+    
+    # Target the search button by id
+    submit_input = driver.find_element(By.ID, "submit_search")
+    # Click on the search button
+    submit_input.click()
 
-# Open the webpage 
-driver.get("https://www.automationexercise.com/products")
+    # Wait for the result to load
+    wait_res = WebDriverWait(driver, 10)
+    wait_res.until(EC.presence_of_element_located((By.ID, "search_product")))
+    time.sleep(5)
 
-# Target the search box by id
-search_input = driver.find_element(By.ID, "search_product")
-# Submit the search term by clicking enter
-search_input.send_keys(user_input)
-submit_input = driver.find_element(By.ID, "submit_search")
-submit_input.click()
+def main():
+    # Request input from user
+    user_input = input("Enter a search term: ")
 
+    try:
+        driver = initialize_driver()
+        # Execute the code through the search_product function
+        search_product(driver, user_input)
+    finally:
+        driver.quit()
 
-
-# Wait for search results to load
-wait_res = WebDriverWait(driver, 10)
-wait_res.until(EC.presence_of_element_located((By.ID, "search_product")))
-
-time.sleep(5)
-
-driver.quit()
-
-
-
-
+if __name__ == "__main__":
+    main()
